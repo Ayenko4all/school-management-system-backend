ea3dba411b2348667b88e878708a6ef9227190db
Ayeni kehinde Oluwatosin <kenny4all40@gmail.com>
2021-11-26 23:03:06 +0100

    update classroom resource

--- app/Http/Resources/ClassroomResource.php
+++ app/Http/Resources/ClassroomResource.php
@@ -23,6 +23,7 @@ class ClassroomResource extends JsonResource
             'created_at'    => $this->created_at,
             'updated_at'    => $this->updated_at,
             'subjects'      => SubjectResource::collection($this->whenLoaded('subjects')),
+            'session'      => new SessionResource($this->whenLoaded('session')),
         ];
     }
 }
